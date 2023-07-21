813dff7691a0017bc70bbd0b7133e7bb58cb0c7b
wilfriedy <wilfriedakpe3@gmail.com>
2023-07-21 19:34:46 +0100

    bookkmarkbtn test

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,5 +1,22 @@
-import { hello } from "./utils.js";
-let btn = document.querySelector("button");
+// import { hello } from "./utils.js";
+// let btn = document.querySelector("button");
 
-console.log("hello");
-hello();
+// console.log("hello");
+// hello();
+document
+  .getElementById("bookmarkButton")
+  .addEventListener("click", function () {
+    // Query for the active tab in the current window
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      var activeTab = tabs[0];
+
+      // Create a new bookmark for the active tab
+      chrome.bookmarks.create({
+        parentId: "1", // this is the id of the "Other bookmarks" folder
+        title: activeTab.title,
+        url: activeTab.url,
+      });
+
+      console.log(activeTab);
+    });
+  });
